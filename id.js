40d68bd4d4a8e4b9b9ef2e31b12e4cda90e78727
abhiram2600 <59743820+abhiram2600@users.noreply.github.com>
2020-03-25 18:40:16 +0530

    uploaded

---
+++ id.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { Pool, Client } = require('pg')
+const app = express()
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "form",
+    password: "johncena",
+    port: 5432
+});
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+//app
+app.post('/submit', urlencodedParser, (req, res) => {
+    let na = req.body.name;
+    let nu = Number(req.body.number);
+    let values = [na, nu]
+    let quer = 'INSERT INTO formdata(name,number)values($1, $2)';
+    //"INSERT INTO formdata(name,number)values('na','nu')"
+    pool.query(quer, values, (err, res) => {
+        console.log(err, res)
+        pool.end();
+    })
+    res.send(`welcome ${na} your number is ${nu} `)
+})
+
+app.listen(3000)
